ca2c316d71bd405ac7e7337c6a3e5c33dec5fe24
Nick <ntomsho4545@gmail.com>
2019-10-28 16:54:47 -0400

    Add responsive styling for inventory section

--- src/dndbags/inventory.jsx
+++ src/dndbags/inventory.jsx
@@ -36,6 +36,7 @@ export default function Inventory(props) {
                 </ul>
                 <div>Plus {8 - itemPackage.length} standard items</div>
                 <button 
+                className="accept-button"
                 // disabled={startingChoices.length === itemPackage.length}
                 onClick={createStartingInv}
                 >
@@ -154,39 +155,69 @@ export default function Inventory(props) {
         <>
         <h2>Inventory</h2>
         {startingEquipmentDisp()}
-        <h3>Carried Gear</h3>
-        <div className="sheet-row">
-            <input className="inventory-space" onChange={handleChange} name="0" type="text" id="carried-1" value={props.inventory[0] || ""}></input>
-            <button onClick={() => moveToStash(0)}>Move to Stash</button>
-            <input className="inventory-space" onChange={handleChange} name="1" type="text" id="carried-2" value={props.inventory[1] || ""}></input>
-            <button onClick={() => moveToStash(1)}>Move to Stash</button>
-            <input className="inventory-space" onChange={handleChange} name="2" type="text" id="carried-3" value={props.inventory[2] || ""}></input>
-            <button onClick={() => moveToStash(2)}>Move to Stash</button>
-        </div>
-        <h3>Stashed Gear</h3>
-        <div className="sheet-row">
-            <input className="inventory-space" onChange={handleChange} name="3" type="text" id="stashed-1" value={props.inventory[3] || ""}></input>
-            <button onClick={() => moveToCarried(3)}>Move to Carried</button>
-            <input className="inventory-space" onChange={handleChange} name="4" type="text" id="stashed-2" value={props.inventory[4] || ""}></input>
-            <button onClick={() => moveToCarried(4)}>Move to Carried</button>
-            <input className="inventory-space" onChange={handleChange} name="5" type="text" id="stashed-3" value={props.inventory[5] || ""}></input>
-            <button onClick={() => moveToCarried(5)}>Move to Carried</button>
-        </div>
-        <div className="sheet-row">
-            <input className="inventory-space" onChange={handleChange} name="6" type="text" id="stashed-4" value={props.inventory[6] || ""}></input>
-            <button onClick={() => moveToCarried(6)}>Move to Carried</button>
-            <input className="inventory-space" onChange={handleChange} name="7" type="text" id="stashed-5" value={props.inventory[7] || ""}></input>
-            <button onClick={() => moveToCarried(7)}>Move to Carried</button>
-            <input className="inventory-space" onChange={handleChange} name="8" type="text" id="stashed-6" value={props.inventory[8] || ""}></input>
-            <button onClick={() => moveToCarried(8)}>Move to Carried</button>
-        </div>
-        <div className="sheet-row">
-            <input className="inventory-space" onChange={handleChange} name="9" type="text" id="stashed-7" value={props.inventory[9] || ""}></input>
-            <button onClick={() => moveToCarried(9)}>Move to Carried</button>
-            <input className="inventory-space" onChange={handleChange} name="10" type="text" id="stashed-8" value={props.inventory[10] || ""}></input>
-            <button onClick={() => moveToCarried(10)}>Move to Carried</button>
-            <input className="inventory-space" onChange={handleChange} name="11" type="text" id="stashed-9" value={props.inventory[11] || ""}></input>
-            <button onClick={() => moveToCarried(11)}>Move to Carried</button>
+        <div className="inventory-container">
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+                <h3>Carried Gear</h3>
+                <div className="inv-row">
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="0" type="text" id="carried-1" value={props.inventory[0] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToStash(0)}>Stash</button>
+                    </div>
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="1" type="text" id="carried-2" value={props.inventory[1] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToStash(1)}>Stash</button>
+                    </div>
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="2" type="text" id="carried-3" value={props.inventory[2] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToStash(2)}>Stash</button>
+                    </div>
+                </div>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+                <h3>Stashed Gear</h3>
+                <div className="inv-row">
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="3" type="text" id="stashed-1" value={props.inventory[3] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToCarried(3)}>Carry</button>
+                    </div>
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="4" type="text" id="stashed-2" value={props.inventory[4] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToCarried(4)}>Carry</button>
+                    </div>
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="5" type="text" id="stashed-3" value={props.inventory[5] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToCarried(5)}>Carry</button>
+                    </div>
+                </div>
+                <div className="inv-row">
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="6" type="text" id="stashed-4" value={props.inventory[6] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToCarried(6)}>Carry</button>
+                    </div>
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="7" type="text" id="stashed-5" value={props.inventory[7] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToCarried(7)}>Carry</button>
+                    </div>
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="8" type="text" id="stashed-6" value={props.inventory[8] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToCarried(8)}>Carry</button>
+                    </div>
+                </div>
+                <div className="inv-row">
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="9" type="text" id="stashed-7" value={props.inventory[9] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToCarried(9)}>Carry</button>
+                    </div>
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="10" type="text" id="stashed-8" value={props.inventory[10] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToCarried(10)}>Carry</button>
+                    </div>
+                    <div className="inv-space-container">
+                        <input className="inventory-space" onChange={handleChange} name="11" type="text" id="stashed-9" value={props.inventory[11] || ""}></input>
+                        <button className="move-item-button" onClick={() => moveToCarried(11)}>Carry</button>
+                    </div>
+                </div>
+            </div>
         </div>
         </>
     )
