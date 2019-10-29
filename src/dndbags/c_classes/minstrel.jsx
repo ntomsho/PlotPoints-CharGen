2c43c8e4cd7880d72f8df6d81e3e8878b5b3e169
Nick <ntomsho4545@gmail.com>
2019-10-29 00:41:33 -0400

    Style minstrel

--- src/dndbags/c_classes/minstrel.jsx
+++ src/dndbags/c_classes/minstrel.jsx
@@ -39,7 +39,7 @@ export default function Minstrel(props) {
             return (
                 <>
                     <div>
-                        <span>Currently Playing: </span><span>{currentSong}</span>
+                        Currently Playing:<br /><strong>{currentSong}</strong>
                     </div>
                     <button onClick={() => setCurrentSong(null)}>End Song</button>
                 </>
@@ -62,16 +62,14 @@ export default function Minstrel(props) {
     function songsDisp() {
         if (currentSpecials.songs) {
             return (
-                <ul sytle={{ listStyle: 'none' }}>
+                <ul className="resource-list">
                     {currentSpecials.songs.map((song, i) => {
                         return (
-                            <div key={i}>
-                                <li>
-                                    <div className={`song${currentSong === song ? ' selected' : ''}`}>{song}</div>
-                                </li>
-                                <button onClick={() => playSong(i)}>Play It!</button>
-                                <button onClick={() => sacrificeSong(i)}>Sacrifice for Crescendo</button>
-                            </div>
+                            <li key={i} className="resource-list-entry">
+                                <div className={`song${currentSong === song ? ' selected' : ''}`}>{song}</div>
+                                <button onClick={() => playSong(i)}>🎵</button>
+                                <button onClick={() => sacrificeSong(i)}>X</button>
+                            </li>
                         )
                     })}
                 </ul>
@@ -84,21 +82,30 @@ export default function Minstrel(props) {
             <div className="class-info">
                 <div className="class-desc">A rockin’ magical songsmith and all-around entertainer.</div>
                 <br />
-                <div>Magic Ability:<br /><strong>Bard Songs</strong></div>
-                <div>Your music is magic! Whenever you start playing one of your songs, you can use its magical effects in any action for the rest of the scene. You can sacrifice a song you aren't playing or end one you are playing to create a Crescendo:</div>
-                <ul>
-                    <li>Create a blast of sound and force from your instrument or voice</li>
-                    <li>Give yourself or an ally Magic Advantage on an action</li>
-                </ul>
+                <div className="ability-desc">
+                    <div>Magic Ability:<br /><strong>Bard Songs</strong></div>
+                    <div>Your music is magic! Whenever you start playing one of your songs, you can use its magical effects in any action for the rest of the scene. You can sacrifice a song you aren't playing or end one you are playing to create a Crescendo:</div>
+                    <ul>
+                        <li>Create a blast of sound and force from your instrument or voice</li>
+                        <li>Give yourself or an ally Magic Advantage on an action</li>
+                    </ul>
+                </div>
             </div>
             <div className="class-ability-display">
-                {currentSongDisp()}
-                <div id="form-list">
+                <div className="ability-main">
+                    {currentSongDisp()}
+                </div>
+                <div className="resource-lists-container" id="form-list">
                     {songsDisp()}
                 </div>
-                <div>
+                <div className="ability-management-container">
+                    <div className="custom-add-row">
+                        <div>Add Song: </div>
+                        <div className="custom-add-field">
+                            <input type="text" ref={input}></input><button onClick={addCustomSong}>+</button>
+                        </div>
+                    </div>
                     <button className="ability-randomize-button" onClick={createSongs}>Randomize Songs</button>
-                    <span>Add Song: </span><input type="text" ref={input}></input><button onClick={addCustomSong}>+</button>
                 </div>
             </div>
         </div>
