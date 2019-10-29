c7e5a025168b785e4169879c53e023e7fa79495c
Nick <ntomsho4545@gmail.com>
2019-10-29 01:29:18 -0400

    Style runegoon

--- src/dndbags/c_classes/runegoon.jsx
+++ src/dndbags/c_classes/runegoon.jsx
@@ -40,15 +40,13 @@ export default function Runegoon(props) {
     function runesDisp() {
         if (currentSpecials.runes) {
             return (
-                <ul style={{ listStyle: 'none' }}>
+                <ul className="resource-list">
                     {currentSpecials.runes.map((r, i) => {
                         return (
-                            <div key={i}>
-                                <li>
-                                    <div><strong>{r.rune}</strong>{r.word}</div>
-                                </li>
-                                <button onClick={() => activateRune(i)}>Inscribe</button>
-                            </div>
+                            <li key={i} className="resource-list-entry">
+                                <div><strong>{r.word}</strong></div>
+                                <button onClick={() => activateRune(i)}><strong>{r.rune}</strong></button>
+                            </li>
                         )
                     })}
                 </ul>
@@ -61,14 +59,12 @@ export default function Runegoon(props) {
             return (
                 <>
                 <h3>Active Runes</h3>
-                <ul style={{ listStyle: 'none' }}>
+                <ul className="resource-list">
                     {activeRunes.map((r, i) => {
                         return (
-                            <div key={i}>
-                                <li>
-                                    <div><strong>{r.rune}</strong>{r.word} on <input type='text' /></div>
-                                </li>
-                            </div>
+                            <li key={i} className="resource-list-entry">
+                                <div>{r.rune} <strong>{r.word}</strong> on <input className="rune-on-input" type='text' /></div>
+                            </li>
                         )
                     })}
                 </ul>
@@ -83,12 +79,15 @@ export default function Runegoon(props) {
             <div className="class-info">
                 <div className="class-desc">A crafter of magical runes that invoke an ancient arcane language.</div>
                 <br />
-                <div>Magic Ability:<br /><strong>Arcane Runes</strong></div>
-                <div>Whenever you rest, you gain access to a set of four ancient runes. You can activate any of them by writing, painting, or inscribing the associated rune onto something.</div>
-                <div>Whatever you draw the rune on is infused with the properties of the runic word for the rest of the scene.</div>
+                <div className="ability-desc">
+                    <div>Magic Ability:<br /><strong>Arcane Runes</strong></div>
+                    <div>Whenever you rest, you gain access to a set of four ancient runes. You can activate any of them by writing, painting, or inscribing the associated rune onto something.</div>
+                    <div>Whatever you draw the rune on is infused with the properties of the runic word for the rest of the scene.</div>
+                </div>
             </div>
             <div className="class-ability-display">
-                <div id="rune-lists" style={{display: 'flex', flexDirection: 'column'}}>
+                {/* <div id="rune-lists" style={{display: 'flex', flexDirection: 'column'}}> */}
+                <div className="resource-lists-container">
                     <div id="active-runes">
                         {activeRunesDisp()}
                     </div>
@@ -96,9 +95,14 @@ export default function Runegoon(props) {
                         {runesDisp()}
                     </div>
                 </div>
-                <div>
+                <div className="ability-management-container">
+                    <div className="custom-add-row">
+                        <div>Add Rune: </div>
+                        <div className="custom-add-field">
+                            <input type="text" ref={input}></input><button onClick={addCustomRune}>+</button>
+                        </div>
+                    </div>
                     <button className="ability-randomize-button" onClick={createRunes}>Generate Random Runes</button>
-                    <span>Add Rune: </span><input type="text" ref={input}></input><button onClick={addCustomRune}>+</button>
                 </div>
             </div>
         </div>
