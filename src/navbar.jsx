ad57034a7a1da237435a8d5baf2f269a3207e971
Nick <ntomsho4545@gmail.com>
2019-11-26 21:18:11 -0500

    Format navbar menu button

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -5,21 +5,35 @@ import { Greetings, NavBar, Nav, NavRight } from 'aws-amplify-react';
 export default class MyNavbar extends Greetings {
     constructor(props) {
         super(props);
+        this.state = {
+            menuOpen: false
+        }
+    }
+
+    toggleMenu() {
+
     }
 
     render() {
         const { currentUser } = this.props;
         const signedIn = !!currentUser;
-
-        // const theme = this.props.theme;
+        
         const greeting = signedIn ? currentUser : "Signed Out";
         if (!greeting) return null;
         
         return (
             <NavBar>
                 <Nav>
-                    {greeting}
+                    <div style={{position: 'relative'}}>
+                        <img id="navbar-menu"
+                            src="https://img.icons8.com/ios-filled/50/000000/menu.png"
+                            onClick={this.toggleMenu}
+                        >
+                        </img>
+                        
+                    </div>
                     <NavRight>
+                        {greeting}
                         <button onClick={() => Auth.signOut()}>Log Out</button>
                     </NavRight>
                 </Nav>
