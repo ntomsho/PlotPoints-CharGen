16bad82974d2c4fd30cf1ccd64f332c06e52ebd4
Nick <ntomsho4545@gmail.com>
2019-12-11 00:43:45 -0500

    Update verbpriest to new rules

--- src/dndbags/c_classes/verbpriest.jsx
+++ src/dndbags/c_classes/verbpriest.jsx
@@ -60,7 +60,7 @@ export default function Verbpriest(props) {
                     <div className="ability-desc-scrollbox">
                         <div>Magic Ability:<br /><strong>Sacred Words</strong></div>
                         <div>Whenever you rest, you recall 6 Words of Power in the ancient language of the gods.</div>
-                        <div>When you speak one of the Words at a creature or object, it is compelled to perform the action until completed or at least attempt to for a few minutes</div>
+                        <div>When you speak one of the Words at a creature or object, it is compelled to attempt to perform the action until completed or, if ongoing, for the duration of the scene.</div>
                         <div>Simple safe commands work automatically but you may have to roll to:</div>
                         <ul>
                             <li>Make a dangerous or self-destructive command of a living creature</li>
